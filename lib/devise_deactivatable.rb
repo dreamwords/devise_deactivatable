98ca5930b84ab5bfb01f0c46253d3f81df19fb98
Elan Meng <dreamwords@gmail.com>
2014-06-04 20:56:19 +0800

    devise_deactivatable model

---
+++ lib/devise_deactivatable.rb
@@ -0,0 +1,3 @@
+require 'devise'
+
+Devise.add_module :deactivatable, :model => 'devise_deactivatable/model'
---
+++ lib/devise_deactivatable/model.rb
@@ -0,0 +1,31 @@
+module Devise
+  module Models
+    module Deactivatable
+      def self.required_fields(klass)
+        attributes = [:deactivated_at]
+        attributes
+      end
+      
+      # Overwrites active_for_authentication? from Devise::Models::Activatable for deactivate purposes
+      # by verifying whether a user is active to sign in or not based on deactivated?
+      def active_for_authentication?
+        !deactivated? && super
+      end
+
+      def deactivate!
+        self.deactivated_at = Time.now
+        save!(:validate => false)
+      end      
+
+      def deactivated?
+        !!deactivated_at
+      end
+      
+      # Overwrites invalid_message from Devise::Models::Authenticatable to define
+      # the correct reason for blocking the sign in.
+      def inactive_message
+        deactivated? ? 'The account has already been deactivated' : super
+      end
+    end
+  end
+end
